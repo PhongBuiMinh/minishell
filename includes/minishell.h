626a0f815db742cd747053cf352d9e780cea8b19
Boris Petrovic <bpetrovi@1-D-23.42heilbronn.de>
2025-10-28 18:15:48 +0100

    small tests

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,8 +6,15 @@
 /*   By: bpetrovi <bpetrovi@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 19:50:25 by bpetrovi          #+#    #+#             */
-/*   Updated: 2025/10/27 20:09:34 by bpetrovi         ###   ########.fr       */
+/*   Updated: 2025/10/27 22:05:52 by bpetrovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-sdfs
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
--- minishell.c
+++ minishell.c
@@ -1,1 +1,23 @@
-hello
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpetrovi <bpetrovi@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 21:09:30 by bpetrovi          #+#    #+#             */
+/*   Updated: 2025/10/27 22:06:01 by bpetrovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+int	main(void)
+{
+	char	*line;
+
+	line = readline("$");
+	printf("%s", line);
+	free(line);
+	return (0);
+}

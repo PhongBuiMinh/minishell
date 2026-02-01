f8cc3fc5626a3afa68c559e737d2d067c2589610
Phong-byte <144224325+PhongBuiMinh@users.noreply.github.com>
2026-02-01 19:28:38 +0000

    clean main function

--- srcs/main.c
+++ srcs/main.c
@@ -10,38 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-volatile sig_atomic_t	g_signal_received = 0;
-
-typedef struct s_cmd_exec
-{
-	char				**argv;
-	int					argc;
-	t_redirection		*redirections;
-}						t_cmd_exec;
-
-typedef struct s_executor
-{
-	int					saved_stdin;
-	int					saved_stdout;
-	int					last_exit_status;
-	t_shell_state		*shell;
-}						t_executor;
-
-typedef struct s_env
-{
-	char				*name;
-	char				*value;
-	struct s_env		*next;
-}						t_env;
-
-typedef struct s_shell_state
-{
-	t_env				*env;
-	int					exit_status;
-	t_command_list		*commands;
-}						t_shell_state;
+#include "exec.h"
 
 int	main(void)
 {

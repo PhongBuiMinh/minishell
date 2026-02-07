bd204d293440b3729340901bbb12a4efb7e3e00a
Phong-byte <144224325+PhongBuiMinh@users.noreply.github.com>
2026-02-07 02:06:00 +0000

    implement execute_external_function

--- includes/exec.h
+++ includes/exec.h
@@ -11,8 +11,6 @@
 # define MAX_ARGS 20
 # define PATH_MAX 20
 
-volatile sig_atomic_t	g_signal_received = 0;
-
 typedef struct s_env
 {
 	char				*name;
@@ -29,6 +27,13 @@ typedef struct s_pipe_info
 	int					is_last;
 }						t_pipe_info;
 
+typedef struct s_shell_state
+{
+	t_env				*env;
+	char				**envp;
+	int					exit_status;
+}						t_shell_state;
+
 typedef struct s_exec_info
 {
 	t_command_list		*cmd;
@@ -38,16 +43,10 @@ typedef struct s_exec_info
 	int					num_cmds;
 }						t_exec_info;
 
-typedef struct s_shell_state
-{
-	t_env				*env;
-	char				**envp;
-	int					exit_status;
-}						t_shell_state;
-
 void					execute_command(t_exec_info *info);
 int						execute_pipeline(t_command_list *commands,
 							t_shell_state *shell);
+char					*find_command_path(char *cmd, t_shell_state *shell);
 
 int						is_builtin(const char *cmd);
 int						handle_builtin(char *cmd, char **argv,
@@ -55,8 +54,19 @@ int						handle_builtin(char *cmd, char **argv,
 int						ft_cd(char **argv, t_shell_state *shell);
 int						ft_echo(char **argv);
 int						ft_env(t_shell_state *shell);
+int						ft_exit(char **argv, t_shell_state *shell);
+int						ft_export(char **args, t_shell_state *shell);
 int						ft_pwd(void);
+int						ft_unset(char **argv, t_shell_state *shell);
+
+void					update_env(t_env **env_list, char *name, char *value);
+t_env					*find_env_var(t_env *env, char *name);
+void					add_env_var(t_env **env_list, char *name, char *value);
 
 int						ft_strcmp(const char *s1, const char *s2);
+
+
+void					run_parser_tests(void);
+void					print_cmd_list(t_command_list *commands);
 
 #endif
---
+++ srcs/executor/path_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbui-min <fbui-min@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 20:33:23 by fbui-min          #+#    #+#             */
+/*   Updated: 2026/02/07 01:08:41 by fbui-min         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+void	ft_free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = -1;
+	while (split[++i])
+		free(split[i]);
+	free(split);
+}
+
+char	*get_path_env(t_shell_state *shell)
+{
+	int	i;
+
+	i = 0;
+	if (!shell->envp)
+		return (NULL);
+	while (shell->envp[i])
+	{
+		if (ft_strncmp(shell->envp[i], "PATH=", 5) == 0)
+			return (shell->envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*build_path(char *dir, char *cmd)
+{
+	char	*tmp;
+	char	*full_path;
+
+	tmp = ft_strjoin(dir, "/");
+	if (!tmp)
+		return (NULL);
+	full_path = ft_strjoin(tmp, cmd);
+	free(tmp);
+	return (full_path);
+}
+
+char	*search_in_paths(char **paths, char *cmd)
+{
+	char	*full_path;
+	int		i;
+
+	i = 0;
+	while (paths[i])
+	{
+		full_path = build_path(paths[i], cmd);
+		if (!full_path)
+			return (NULL);
+		if (access(full_path, X_OK) == 0)
+			return (ft_free_split(paths), full_path);
+		free(full_path);
+		i++;		
+	}
+	ft_free_split(paths);
+	return (NULL);
+}
+
+char	*find_command_path(char *cmd, t_shell_state *shell)
+{
+	char	*path_env;
+	char	**paths;
+	
+	// dprintf(2, "DEBUG: find_command_path for '%s'\n", cmd);
+	if (ft_strchr(cmd, '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+			return (ft_strdup(cmd));
+		return (NULL);
+	}
+	path_env = get_path_env(shell);
+	// dprintf(2, "DEBUG: PATH='%s'\n", path_env ? path_env : "NULL");
+	if (!path_env)
+		return (NULL);
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		return (NULL);
+	return (search_in_paths(paths, cmd));
+}

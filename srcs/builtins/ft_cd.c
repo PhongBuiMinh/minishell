e1362978158753960107ed81c48a02bedfef43b5
fbui-min <fbui-min@student.42heilbronn.de>
2026-03-18 10:20:48 +0100

    updaes the oldpwd, pwd env variables so cd - work correctly

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -23,26 +23,39 @@ char	*ft_getenv(t_env *env, char *name)
 	return (NULL);
 }
 
+void	update_work_dirs(t_shell_state *shell, char *old_path)
+{
+	char	new_path[PATH_MAX];
+
+	update_env(&shell->env, "OLDPWD", old_path);
+	if (getcwd(new_path, sizeof(new_path)))
+		update_env(&shell->env, "PWD", new_path);
+}
+
 int	ft_cd(char **argv, t_shell_state *shell)
 {
 	char	*path;
-	char	cwd[PATH_MAX];
+	char	old_cwd[PATH_MAX];
 
 	if (argv[1] && argv[2])
 		return (ft_putstr_fd("minishell: cd: too many arguments\n", 2), 1);
 	if (!argv[1] || ft_strcmp(argv[1], "~") == 0)
 		path = ft_getenv(shell->env, "HOME");
+	else if (ft_strcmp(argv[1], "-") == 0)
+	{
+		path = ft_getenv(shell->env, "OLDPWD");
+		if (!path)
+			return (ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2), 1);
+		ft_putendl_fd(path, STDOUT_FILENO);
+	}
 	else
 		path = argv[1];
 	if (!path)
 		return (ft_putstr_fd("minishell: cd: HOME not set\n", 2), 1);
-	if (!getcwd(cwd, sizeof(cwd)))
-		return (perror("minishell: cd"), 1);
-	update_env(&shell->env, "OLDPWD", cwd);
+	if (!getcwd(old_cwd, sizeof(old_cwd)))
+		old_cwd[0] = '\0';
 	if (chdir(path) != 0)
 		return (perror("minishell: cd"), 1);
-	if (!getcwd(cwd, sizeof(cwd)))
-		return (perror("minishell: cd"), 1);
-	update_env(&shell->env, "PWD", cwd);
+	update_work_dirs(shell, old_cwd);
 	return (0);
 }
